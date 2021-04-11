61190a85246d56568a4f6791b48b4a150d60424e
Amad-Mlinka <amadovski@gmail.com>
2021-04-11 22:21:08 +0200

    First Commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import './Header.css'
+
+export const Header = () => {
+    const[hamburger, setHamburger] = useState(false)
+    const onClickHamburger = () => {
+        setHamburger(!hamburger);
+    }
+    const onClick = () => {
+        setHamburger(!hamburger);
+    }
+    return (
+        <header className="header">
+            <nav className="navbar">
+                <a href="#" className="nav-logo">Test</a>
+                <ul className ={`nav-menu ${hamburger && "active"}`}>
+                    <li className="nav-item" onClick={onClick}>
+                        <a href="#" className="nav-link">Services</a>
+                    </li>
+                    <li className="nav-item" onClick={onClick}>
+                        <a href="#" className="nav-link">Blog</a>
+                    </li>
+                    <li className="nav-item" onClick={onClick}>
+                        <a href="#" className="nav-link">About</a>
+                    </li>
+                    <li className="nav-item" onClick={onClick}>
+                        <a href="#" className="nav-link">Contact</a>
+                    </li>
+                </ul>
+                <div className={`hamburger ${hamburger && "active"}`} onClick={onClickHamburger}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+            </nav>
+
+        </header>
+    )
+}
