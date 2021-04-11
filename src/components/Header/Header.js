757f3af9d3323bff25fa227c736800a0d88cf193
Amad-Mlinka <amadovski@gmail.com>
2021-04-11 22:40:34 +0200

    Added react bootstrap css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,28 +11,31 @@ export const Header = () => {
     }
     return (
         <header className="header">
-            <nav className="navbar">
-                <a href="#" className="nav-logo">Test</a>
-                <ul className ={`nav-menu ${hamburger && "active"}`}>
-                    <li className="nav-item" onClick={onClick}>
-                        <a href="#" className="nav-link">Services</a>
-                    </li>
-                    <li className="nav-item" onClick={onClick}>
-                        <a href="#" className="nav-link">Blog</a>
-                    </li>
-                    <li className="nav-item" onClick={onClick}>
-                        <a href="#" className="nav-link">About</a>
-                    </li>
-                    <li className="nav-item" onClick={onClick}>
-                        <a href="#" className="nav-link">Contact</a>
-                    </li>
-                </ul>
-                <div className={`hamburger ${hamburger && "active"}`} onClick={onClickHamburger}>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                </div>
-            </nav>
+            <div className="container">
+                <nav className="navbar">
+                    <a href="#" className="nav-logo">Test</a>
+                    <ul className ={`nav-menu ${hamburger && "active"}`}>
+                        <li className="nav-item" onClick={onClick}>
+                            <a href="#" className="nav-link">Services</a>
+                        </li>
+                        <li className="nav-item" onClick={onClick}>
+                            <a href="#" className="nav-link">Blog</a>
+                        </li>
+                        <li className="nav-item" onClick={onClick}>
+                            <a href="#" className="nav-link">About</a>
+                        </li>
+                        <li className="nav-item" onClick={onClick}>
+                            <a href="#" className="nav-link">Contact</a>
+                        </li>
+                    </ul>
+                    <div className={`hamburger ${hamburger && "active"}`} onClick={onClickHamburger}>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                    </div>
+                </nav>
+            </div>
+            
 
         </header>
     )
